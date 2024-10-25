9966d90c4b58a0ced2b9ec8815dd6bfa1a059661
Your Name <you@example.com>
2024-10-25 12:40:54 +0200

    Modification de l'ordre de tri des événements pour un défilement du plus récent au plus ancien. - Ajout de la mise à jour automatique de l'index du slider toutes les 5 secondes.

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -6,7 +6,6 @@ import "./style.scss";
 
 const Slider = () => {
   const { data } = useData();
-  console.log(data);
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
